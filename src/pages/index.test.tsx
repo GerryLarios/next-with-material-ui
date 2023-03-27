72390d8b02bae8af36643efe854c2e2317d64dcc
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2023-03-27 09:44:38 -0600

    chore: setup-jest

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import Index from './index';
+
+describe('Index', () => {
+  it('should render correctly', () => {
+    render(<Index />);
+  });
+});
