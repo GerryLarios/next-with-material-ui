3f13621dec66b6cd0056148ec1f5007887666210
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2023-03-27 11:22:05 -0600

    chore: change paths

--- src/pages/api/hello.api.ts
+++ src/pages/api/hello.api.ts
@@ -1,10 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
+  id: string;
   name: string;
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json({
+    id: '1',
+    name: 'Gerry Larios',
+  });
 }
